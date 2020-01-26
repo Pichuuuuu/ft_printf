69c5da67f8d277891c71063ef0ca43963f20ca92
tamather <taygergix@gmail.com>
2020-01-26 20:11:22 +0100

    ssss

--- src/printf.c
+++ src/printf.c
@@ -6,13 +6,13 @@
 /*   By: tamather <tamather@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 12:29:52 by tamather          #+#    #+#             */
-/*   Updated: 2020/01/16 15:04:58 by tamather         ###   ########.fr       */
+/*   Updated: 2020/01/26 17:53:38 by tamather         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int arg_count(const char *arg)
+int 	arg_count(const char *arg)
 {
 	int i;
 	int c;
@@ -21,20 +21,24 @@ int arg_count(const char *arg)
 	c = 0;
 	while(arg[i])
 	{
-		if (arg[i] == '%')
-			c++;
+		if (arg[i] == '%' && arg[i + 1] != '%')
+			if (arg[i + 1] == 'c' || arg[i + 1] == 's' || arg[i + 1] == 'p'|| arg[i + 1] == 'i' 
+				|| arg[i + 1] == 'u' || arg[i + 1] == 'x' || arg[i + 1] == 'X')
+				c++;
 		i++;
 	}
 	return(c);
 }
 
+int		pf_print()
+
 int		ft_printf(const char *arg, ...)
 {
 	va_list	list;
 	int		result;
-	printf("%d", arg_count(arg));
+	printf("%d\n\n", arg_count(arg));
 	va_start(list, arg_count(arg));
-	
+
 	va_end(list);
 	return (result);
 }
