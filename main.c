7f3c3483e132193d5cd7068b4719f5c0ab3bf34e
tamather <taygergix@gmail.com>
2020-02-13 09:35:37 +0100

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tamather <tamather@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 01:19:15 by tamather          #+#    #+#             */
-/*   Updated: 2020/02/13 08:54:26 by tamather         ###   ########.fr       */
+/*   Updated: 2020/02/13 09:21:55 by tamather         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int main(int argc, char const *argv[])
 {
 	char test[] = "esdsfef";
 	int test1 = -555444;
-	int test2 = 54;
+	double test2 = 54.3;
 	//ft_printf(argv[1]);
-	printf("|%0.*i| |%*i|", 700, test1, 5, test2);
+	printf("|%04.*i| |%*f|", 6, test1, 5, test2);
 	//printf("%d", ft_isalnum('6'));
 	return 0;
 }
